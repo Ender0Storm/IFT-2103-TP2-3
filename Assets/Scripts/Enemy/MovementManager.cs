8fb9548e5adbcd2017aa709ae63428fc2f0e1544
Fermalt <florent.kerloch@laposte.net>
2023-11-09 11:32:10 -0500

    PathFinding and enemy movement

---
+++ Assets/Scripts/Enemy/MovementManager.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using pathFinding;
+using UnityEngine;
+
+namespace enemy
+{
+    public class MovementManager : MonoBehaviour
+    {
+        private List<Tile> _path;
+        private PathFinding _pathFinding;
+        public float speed;
+        private Tile _nextTile;
+        public int indexPosition;
+        private float _directionX;
+        private float _directionY;
+        public GameObject town;
+        private float _nextPositionX;
+        private float _nextPositionY;
+
+        public void Start()
+        {
+            town = GameObject.Find("Town");
+            GetPath();
+            SetDirection();
+        }
+
+        public void Update()
+        {
+            transform.position = new Vector2(transform.position.x + _directionX, transform.position.y + _directionY);
+
+            if (transform.position.x >= _nextPositionX-0.05 
+                && transform.position.y >= _nextPositionY-0.05
+                && transform.position.x <= _nextPositionX+0.05
+                && transform.position.y <= _nextPositionY+0.05)
+            {
+                Debug.Log(_nextPositionX + " "+ _nextPositionY);
+                transform.position = new Vector2(_nextPositionX, _nextPositionY);
+                if (indexPosition == _path.Count - 1)
+                {
+                    Debug.Log("FINISH");
+                    Destroy(this);
+                }
+                indexPosition++;
+                SetDirection();
+            }
+        }
+
+        private void GetPath()
+        {
+            Tile start = SetStartTile();
+            Tile finish = SetFinishTile();
+            start.SetDistance(finish.X, finish.Y);
+            
+            _pathFinding = new PathFinding(start, finish);
+            
+            _path = _pathFinding.FindPath();
+        }
+
+        private Tile SetStartTile()
+        {
+            Tile start = new Tile
+            {
+                X = (int) transform.position.x,
+                Y = (int) transform.position.y,
+            };
+            
+            return start;
+        }
+
+        private Tile SetFinishTile()
+        {
+            Tile finish = new Tile
+            {
+                X = (int)town.transform.position.x,
+                Y = (int)town.transform.position.y,
+            };
+            return finish;
+        }
+
+        private void SetDirection()
+        {
+            _nextTile = _path[indexPosition];
+            _nextPositionX = _nextTile.X + 0.5f;
+            _nextPositionY = _nextTile.Y + 0.5f;
+            _directionX = (_nextPositionX - transform.position.x) / speed;
+            _directionY = (_nextPositionY - transform.position.y) / speed;
+        }
+    }
+}
---
+++ Assets/Scripts/PathFinding/AccessiblePositionsFinder.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace pathFinding
+{
+    public class AccessiblePositionsFinder
+    {
+        private SpriteRenderer _spriteRenderer;
+        private CollisionManager _collisionManager;
+        private List<Tile> _accessibleTiles;
+        private const float TileRange = 0.5f;
+
+        public List<Tile> GetAccessibleTiles(Tile currentTile, Tile targetTile)
+        {
+            _spriteRenderer = GetMapLimits();
+            _collisionManager = new CollisionManager();
+            _accessibleTiles = new List<Tile>();
+            
+            CardinalPositions(currentTile);
+            DiagonalPositions(currentTile);
+            _accessibleTiles.ForEach(tile => tile.SetDistance(targetTile.X, targetTile.Y));
+            return _accessibleTiles;
+        }
+
+        private void CardinalPositions(Tile currentTile)
+        {
+            var cardinalTiles = new List<Tile>
+            {
+                new Tile { X = currentTile.X, Y = currentTile.Y + TileRange, Parent = currentTile, Cost = currentTile.Cost + TileRange }, //North
+                new Tile { X = currentTile.X, Y = currentTile.Y - TileRange, Parent = currentTile, Cost = currentTile.Cost + TileRange }, //South
+                new Tile { X = currentTile.X + TileRange, Y = currentTile.Y, Parent = currentTile, Cost = currentTile.Cost + TileRange }, //East
+                new Tile { X = currentTile.X - TileRange, Y = currentTile.Y, Parent = currentTile, Cost = currentTile.Cost + TileRange }, //West
+            };
+            
+            foreach(var tile in cardinalTiles)
+            {
+                if (!_collisionManager.IsSquareColliding(tile, TileRange) &&
+                    tile.X > _spriteRenderer.bounds.min.x &&
+                    tile.X < _spriteRenderer.bounds.max.x &&
+                    tile.Y > _spriteRenderer.bounds.min.y &&
+                    tile.Y < _spriteRenderer.bounds.max.y)
+                {
+                    _accessibleTiles.Add(tile);
+                }
+            }
+        }
+
+        private void DiagonalPositions(Tile currentTile)
+        {
+            var diagonalTiles = new List<Tile>();
+
+            if (_accessibleTiles.Any(x => x.X == currentTile.X + TileRange && x.Y == currentTile.Y) && //N
+                _accessibleTiles.Any(x => x.X == currentTile.X && x.Y == currentTile.Y + TileRange)) //E
+            {
+                diagonalTiles.Add(new Tile
+                    { X = currentTile.X + TileRange, Y = currentTile.Y + TileRange, Parent = currentTile, Cost = currentTile.Cost + TileRange }); //N-E
+            }
+            if (_accessibleTiles.Any(x => x.X == currentTile.X + TileRange && x.Y == currentTile.Y) && //N
+                _accessibleTiles.Any(x => x.X == currentTile.X && x.Y == currentTile.Y - TileRange)) //W
+            {
+                diagonalTiles.Add(new Tile
+                    { X = currentTile.X + TileRange, Y = currentTile.Y - TileRange, Parent = currentTile, Cost = currentTile.Cost + TileRange }); //N-W
+            }
+            if (_accessibleTiles.Any(x => x.X == currentTile.X - TileRange && x.Y == currentTile.Y) && //S
+                _accessibleTiles.Any(x => x.X == currentTile.X && x.Y == currentTile.Y + TileRange)) //E
+            {
+                diagonalTiles.Add(new Tile
+                    { X = currentTile.X - TileRange, Y = currentTile.Y + TileRange, Parent = currentTile, Cost = currentTile.Cost + TileRange }); //S-E
+            }
+            if (_accessibleTiles.Any(x => x.X == currentTile.X - TileRange && x.Y == currentTile.Y) && //S
+                _accessibleTiles.Any(x => x.X == currentTile.X && x.Y == currentTile.Y - TileRange)) //W
+            {
+                diagonalTiles.Add(new Tile
+                    { X = currentTile.X - TileRange, Y = currentTile.Y - TileRange, Parent = currentTile, Cost = currentTile.Cost + TileRange }); //S-W
+            }
+            
+            foreach(var tile in diagonalTiles)
+            {
+                if (!_collisionManager.IsSquareColliding(tile, TileRange) &&
+                    tile.X > _spriteRenderer.bounds.min.x &&
+                    tile.X < _spriteRenderer.bounds.max.x &&
+                    tile.Y > _spriteRenderer.bounds.min.y &&
+                    tile.Y < _spriteRenderer.bounds.max.y)
+                {
+                    _accessibleTiles.Add(tile);
+                }
+            }
+        }
+
+        private static SpriteRenderer GetMapLimits()
+        {
+            var ground = GameObject.Find("Ground");
+            return ground.GetComponent<SpriteRenderer>();
+        }
+    }
+}

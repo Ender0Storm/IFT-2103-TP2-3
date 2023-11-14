622d63b21428b764a8338756786e803d70072920
Maxime Corriveau-Faucher <BeatByACube@hotmail.fr>
2023-11-14 13:07:55 -0500

    oups, forgor

--- Assets/Scripts/Game/Enemy/Enemy.cs
+++ Assets/Scripts/Game/Enemy/Enemy.cs
@@ -11,7 +11,7 @@ public class Enemy : MonoBehaviour
     public void DealDamage(int damage)
     {
         health -= damage;
-        if (health < 0)
+        if (health <= 0)
         {
             Destroy(gameObject);
         }

6c2480601ee08ba9742e8d120bdb43d0be0f7108
Fermalt <florent.kerloch@laposte.net>
2023-11-15 01:50:41 -0500

    Menu

---
+++ Assets/Scripts/Game/Menu/AnimateButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Game.Menu
+{
+    public class AnimateButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        private Image _image;
+
+        void Start()
+        {
+            _image = GetComponentInChildren<Image>();
+        }
+        
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            _image.color = new Color(0,150,0);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            _image.color = new Color(255,255,255);
+        }
+    }
+}
---
+++ Assets/Scripts/Game/Menu/Navigation.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Game.Menu
+{
+    public class Navigation : MonoBehaviour
+    {
+        private GameObject _startButton;
+        private GameObject _singleplayerButton;
+        private GameObject _multiplayerButton;
+        private GameObject _optionsButton;
+        private GameObject _back21Button;
+        void Start()
+        {
+            _startButton = GameObject.Find("Start");
+            _singleplayerButton = GameObject.Find("Singleplayer");
+            _multiplayerButton = GameObject.Find("Multiplayer");
+            _optionsButton = GameObject.Find("Options");
+            _back21Button = GameObject.Find("Back");
+            MenuPage2ToMenuPage1();
+        }
+        public void MenuPage1ToMenuPage2()
+        {
+            _startButton.SetActive(false);
+            _singleplayerButton.SetActive(true);
+            _multiplayerButton.SetActive(true);
+            _optionsButton.SetActive(true);
+            _back21Button.SetActive(true);
+            ResetColor();
+        }
+
+        public void MenuPage2ToMenuPage1()
+        {
+            _singleplayerButton.SetActive(false);
+            _multiplayerButton.SetActive(false);
+            _optionsButton.SetActive(false);
+            _back21Button.SetActive(false);
+            _startButton.SetActive(true);
+            ResetColor();
+        }
+
+        public void MenuPage2ToSinglePlayer()
+        {
+            SceneManager.LoadScene("SinglePlayer Scene");
+        }
+
+        public void ResetColor()
+        {
+            _back21Button.GetComponentInChildren<Image>().color = new Color(255,255,255);
+            _startButton.GetComponentInChildren<Image>().color = new Color(255,255,255);
+                
+        }
+    }
+}

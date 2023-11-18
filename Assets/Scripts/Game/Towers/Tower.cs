5c6ce9424ef044901f0ae00b781059e5707680cd
Maxime Corriveau-Faucher <beatbyacube@hotmail.fr>
2023-11-18 16:07:58 -0500

    wall tower

--- Assets/Scripts/Game/Towers/Tower.cs
+++ Assets/Scripts/Game/Towers/Tower.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Tower : MonoBehaviour
+public class Tower : MonoBehaviour
 {
     [SerializeField]
     protected int cost;

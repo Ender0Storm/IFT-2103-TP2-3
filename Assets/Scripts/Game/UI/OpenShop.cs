720501b767c78c0a0cf04a2463dfe34e2b1e5b3a
Fermalt <florent.kerloch@laposte.net>
2023-12-07 10:43:17 -0500

    board customization working

--- Assets/Scripts/Game/UI/OpenShop.cs
+++ Assets/Scripts/Game/UI/OpenShop.cs
@@ -1,10 +1,16 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using Game.Shop;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Game.ui
 {
     public class OpenShop : MonoBehaviour
     {
         public static bool isShopping;
+        public SpriteRenderer customBoard;
+        public Sprite woodBoard;
+        public Sprite digitalBoard;
     
         [SerializeField]
         private GameObject shopMenuUI;
@@ -23,9 +29,38 @@ namespace Game.ui
     
         public void Resume()
         {
+            UpdateContent();
             shopMenuUI.SetActive(false);
             Time.timeScale = 1f;
             isShopping = false;
+        }
+
+        private void UpdateContent()
+        {
+            List<Content> boards = ShopContent.GetContentsByType(ContentType.Board);
+            bool customBoardActive = false;
+            foreach (Content board in boards)
+            {
+                if (board.Activated)
+                {
+                    if (board.Name == "Wood")
+                    {
+                        customBoard.sprite = woodBoard;
+                        customBoardActive = true;
+                        break;
+                    } else if (board.Name == "Digital")
+                    {
+                        customBoard.sprite = digitalBoard;
+                        customBoardActive = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!customBoardActive)
+            {
+                customBoard = null;
+            }
         }
     }
 }

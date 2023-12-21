fa77b8d6d94547733f80d80cd95030b4e8fccdd7
Cl0ut1eR <etiennecloutier00@gmail.com>
2023-12-21 16:12:41 -0500

    fix big mistake

--- Assets/Scripts/Game/PlayerInformation/PlayerPrefsKeys.cs
+++ Assets/Scripts/Game/PlayerInformation/PlayerPrefsKeys.cs
@@ -2,7 +2,7 @@ public static class PlayerPrefsKey
 {
     public const string MASTER_VOLUME_KEY = "masterVolume";
     public const string MUSIC_VOLUME_KEY = "musicVolume";
-    public const string FOLEY_VOLUME_KEY = "musicVolume";
+    public const string FOLEY_VOLUME_KEY = "foleyVolume";
     public const string SFX_VOLUME_KEY = "sfxVolume";
     public const string SEED_KEY = "seed";
     public const string HARD_MODE = "difficulty";
